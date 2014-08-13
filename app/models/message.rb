a8e25426cfaf5167addfba6576739ae2d9fe61f1
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-13 16:26:07 +0900

    Build relation between User and Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ActiveRecord::Base
+
+  belongs_to :user, primary_key: 'uid', foreign_key: 'from_uid'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :messages, primary_key: 'uid', foreign_key: 'from_uid'
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
