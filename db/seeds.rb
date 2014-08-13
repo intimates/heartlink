c10e7fa929c787d526c6388afe1bae20389c9ce7
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-13 19:32:46 +0900

    Create db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'Annette Arnold', uid: '1')
+User.create(name: 'Debbie Snyder', uid: '2')
+User.create(name: 'Rosa Mason', uid: '3')
+
+my_uid = '688784467878364'
+
+Message.create(from_uid: my_uid, to_uid: '1', body: 'me -> 1')
+Message.create(from_uid: my_uid, to_uid: '2', body: 'me -> 2')
+Message.create(from_uid: my_uid, to_uid: '3', body: 'me -> 3')
+Message.create(from_uid: '1', to_uid: my_uid, body: '1 -> me')
+Message.create(from_uid: '2', to_uid: my_uid, body: '2 -> me')
+Message.create(from_uid: '3', to_uid: my_uid, body: '3 -> me')
