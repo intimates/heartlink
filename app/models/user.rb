9871c59d57713e8eb1c0d85a8a5b2d9e314ab9bd
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-13 19:31:16 +0900

    User has messages and sent messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
-  has_many :messages, primary_key: 'uid', foreign_key: 'from_uid'
+  has_many :messages, primary_key: 'uid', foreign_key: 'to_uid'
+  has_many :sent_messages, class_name: 'Message', primary_key: 'uid', foreign_key: 'from_uid'
 
   def self.create_with_omniauth(auth)
     create! do |user|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,10 +11,15 @@ describe User do
   end
 
   describe 'relation with Message' do
-    let!(:message) { FactoryGirl.create(:message, from_uid: @user.uid) }
+    let!(:message) { FactoryGirl.create(:message, to_uid: @user.uid) }
+    let!(:sent_message) { FactoryGirl.create(:message, from_uid: @user.uid) }
 
     it 'has many messages' do
       expect(@user.messages.first).to eql(message)
+    end
+
+    it 'has many sent messages' do
+      expect(@user.sent_messages.first).to eql(sent_message)
     end
   end
 
