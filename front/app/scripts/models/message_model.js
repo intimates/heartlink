6f1be91f6be3892d2650c99f1e0d73f3c95d2d73
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-14 17:57:43 +0900

    List fixture messages

--- front/app/scripts/models/message_model.js
+++ front/app/scripts/models/message_model.js
@@ -1,5 +1,8 @@
 /*global Ember*/
-Front.Message = DS.Model.extend({});
+Front.Message = DS.Model.extend({
+  body: DS.attr('string'),
+  sent_at: DS.attr('date')
+});
 
 // probably should be mixed-in...
 Front.Message.reopen({
@@ -13,15 +16,15 @@ Front.Message.reopen({
 
 // delete below here if you do not want fixtures
 Front.Message.FIXTURES = [
-  
   {
     id: 0,
-    
+    body: "Hello",
+    sent_at: new Date()
   },
-  
+
   {
     id: 1,
-    
+    body: "Nice to meet you.",
+    sent_at: new Date()
   }
-  
 ];
