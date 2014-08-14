d088f37a7f814ef50c1a2320f503cf8683eb3bb6
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-14 21:59:37 +0900

    Return serialized user model

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -3,16 +3,12 @@ class Api::V1::UsersController < Api::V1::ApplicationController
 
   def index
     @users = User.all
-    respond_to do |format|
-      format.json { render json: @users, status: :ok }
-    end
+    render json: @users, status: :ok
   end
 
   def show
     @user = User.find(params[:id])
-    respond_to do |format|
-      format.json { render json: @users, status: :ok }
-    end
+    render json: @user, status: :ok
   end
 
 end
