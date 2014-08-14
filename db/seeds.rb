bd9d1ec8d1e77ca285d7e8a8447e26d7a539c23d
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-14 14:34:25 +0900

    Add pn_jp_words

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'csv'
+
 User.create(name: 'Annette Arnold', uid: '1')
 User.create(name: 'Debbie Snyder', uid: '2')
 User.create(name: 'Rosa Mason', uid: '3')
@@ -18,3 +20,8 @@ Message.create(from_uid: my_uid, to_uid: '3', body: 'me -> 3')
 Message.create(from_uid: '1', to_uid: my_uid, body: '1 -> me')
 Message.create(from_uid: '2', to_uid: my_uid, body: '2 -> me')
 Message.create(from_uid: '3', to_uid: my_uid, body: '3 -> me')
+
+CSV.foreach(File.expand_path('../pn_ja_words.csv', __FILE__), 'r') do |row|
+  word, kana, pos, value = row
+  PnJpWord.create(word: word, kana: kana, pos: pos, value: value)
+end
