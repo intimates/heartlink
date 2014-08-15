8bcb86f0a8699637e1ac29d90dbfdc12e6168f21
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-15 13:26:42 +0900

    Start merging Leo's code

--- front/app/scripts/models/message_model.js
+++ front/app/scripts/models/message_model.js
@@ -2,7 +2,24 @@
 Front.Message = DS.Model.extend({
   body: DS.attr('string'),
   pn_value: DS.attr('number'),
-  sent_at: DS.attr('date')
+  sent_at: DS.attr('date'),
+
+  sentTime: function() {
+    var date = this.get('sent_at');
+    return moment(date).format('HH:mm');
+  }.property('sent_at'),
+
+  elementId: function() {
+    return "message" + this.get('id');
+  }.property('id'),
+
+  pnValueId: function() {
+    return "pn_value" + this.get('id');
+  }.property('id'),
+
+  timeId: function() {
+    return "time" + this.get('id')
+  }.property('id')
 });
 
 // probably should be mixed-in...
