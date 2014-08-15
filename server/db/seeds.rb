df8a21985aa35f92527f4818822b6caf8cd975a6
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-15 13:22:50 +0900

    Create messages with sent_at and create myself as well

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -13,13 +13,14 @@ User.create(name: 'Debbie Snyder', uid: '2')
 User.create(name: 'Rosa Mason', uid: '3')
 
 my_uid = '688784467878364'
+User.create(name: 'Genki Sugimoto', provider: 'facebook', uid: my_uid)
 
-Message.create(from_uid: my_uid, to_uid: '1', body: 'me -> 1')
-Message.create(from_uid: my_uid, to_uid: '2', body: 'me -> 2')
-Message.create(from_uid: my_uid, to_uid: '3', body: 'me -> 3')
-Message.create(from_uid: '1', to_uid: my_uid, body: '1 -> me')
-Message.create(from_uid: '2', to_uid: my_uid, body: '2 -> me')
-Message.create(from_uid: '3', to_uid: my_uid, body: '3 -> me')
+Message.create(from_uid: my_uid, to_uid: '1', body: 'me -> 1', sent_at: Time.parse('08:00'))
+Message.create(from_uid: my_uid, to_uid: '2', body: 'me -> 2', sent_at: Time.parse('12:00'))
+Message.create(from_uid: my_uid, to_uid: '3', body: 'me -> 3', sent_at: Time.parse('18:00'))
+Message.create(from_uid: '1', to_uid: my_uid, body: '1 -> me', sent_at: Time.parse('08:00'))
+Message.create(from_uid: '2', to_uid: my_uid, body: '2 -> me', sent_at: Time.parse('12:00'))
+Message.create(from_uid: '3', to_uid: my_uid, body: '3 -> me', sent_at: Time.parse('18:00'))
 
 CSV.foreach(File.expand_path('../pn_ja_words.csv', __FILE__), 'r') do |row|
   word, kana, pos, value = row
