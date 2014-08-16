0cc0c0b08dd07b51f15256a1b55241e54d8d55a3
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-16 15:40:08 +0900

    Delete messages through API

--- front/app/scripts/controllers/message_controller.js
+++ front/app/scripts/controllers/message_controller.js
@@ -1,4 +1,26 @@
 Front.MessageController = Ember.ObjectController.extend({
-  // Implement your controller here.
+  actions: {
+    deleteRecord: function(message) {
+      // does not work, so jQuery for now
+      // message.destroyRecord();
+
+      message.deleteRecord();
+      var controller = this;
+
+      $.ajax({
+        url: 'http://localhost:3000/api/v1/messages/' + message.id,
+        type: 'delete',
+        headers: {
+          Authorization: 'Token token=688784467878364'
+        },
+        statusCode: {
+          200: function() {
+            alert('message was successfully deleted.');
+            controller.transitionToRoute('messages');
+          }
+        }
+      });
+    }
+  }
 });
 
