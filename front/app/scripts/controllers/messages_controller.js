4c455b73211de920a7eb7e3c1676480fd0f9d3b1
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-16 16:49:21 +0900

    Fire GET request when opening a message

--- front/app/scripts/controllers/messages_controller.js
+++ front/app/scripts/controllers/messages_controller.js
@@ -1,4 +1,9 @@
 Front.MessagesController = Ember.ObjectController.extend({
-  // Implement your controller here.
+  actions: {
+    openMessage: function(message) {
+      // fire GET request and set opened_at on server
+      message.reload();
+      this.transitionToRoute('message', message);
+    }
+  }
 });
-
