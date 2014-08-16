0977bd469000de8d3b939ca5c0a7f92af85d1619
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-16 16:04:58 +0900

    Remove fixtures

--- front/app/scripts/models/message_model.js
+++ front/app/scripts/models/message_model.js
@@ -31,18 +31,3 @@ Front.Message.reopen({
     });
   }.property()
 });
-
-// delete below here if you do not want fixtures
-Front.Message.FIXTURES = [
-  {
-    id: 0,
-    body: "Hello",
-    sent_at: new Date()
-  },
-
-  {
-    id: 1,
-    body: "Nice to meet you.",
-    sent_at: new Date()
-  }
-];
