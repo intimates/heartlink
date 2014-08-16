67053c33701fdabe98c7d9e04b7766a2c1432ed0
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-16 13:41:58 +0900

    Filter users in messages/create

--- front/app/scripts/controllers/messages_create_controller.js
+++ front/app/scripts/controllers/messages_create_controller.js
@@ -1,2 +1,11 @@
-Front.MessagesCreateController = Ember.ObjectController.extend({
+Front.MessagesCreateController = Ember.ArrayController.extend({
+  filteredUsers: function() {
+    var searchTerm = this.get('searchTerm');
+    var regExp = new RegExp(searchTerm, 'i');
+
+    var filteredResults = this.filter(function(user) {
+      return regExp.test(user.get('name'));
+    });
+    return filteredResults;
+  }.property('@each.name', 'searchTerm')
 });
--- front/app/scripts/routes/messages_create_route.js
+++ front/app/scripts/routes/messages_create_route.js
@@ -1,2 +1,5 @@
 Front.MessagesCreateRoute = Ember.Route.extend({
+  model: function() {
+    return this.get('store').find('user');
+  }
 });
