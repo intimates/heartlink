e49ac330c27afb6d15e7ee157a78b84b33ebec80
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-19 10:56:09 +0900

    Implement refresh button

--- front/app/scripts/controllers/messages_controller.js
+++ front/app/scripts/controllers/messages_controller.js
@@ -2,6 +2,11 @@ Front.MessagesController = Ember.ObjectController.extend({
   needs: ['application'],
 
   actions: {
+    refresh: function() {
+      this.transitionToRoute('loading');
+      this.transitionToRoute('messages');
+    },
+
     openMessage: function(message) {
       // fire GET request and set opened_at on server
       message.reload();
