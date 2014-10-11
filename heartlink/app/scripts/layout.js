501509598beb562e0702f6020f9645dc8078a434
Leonardo <leoanrdo.mbc17@gmail.com>
2014-10-12 08:27:01 +0900

    messageのidを'-'に変更、layout.jsを修正

--- heartlink/app/scripts/layout.js
+++ heartlink/app/scripts/layout.js
@@ -1,13 +1,11 @@
-(function($, Vue) {
+(function() {
   'use strict';
 
   initLayout();
   
-})(jQuery, Vue);
-
-function initLayout() {
-  
-  // Background Settings.
-  $("body").css({height: $(window).height() * 3});
-  // TODO: ここに背景の星とか、かもめとかを設置するコードも記載したい。
-}
+  function initLayout() {
+    // Background Settings.
+    $("body").css({height: $(window).height() * 3});
+    // TODO: ここに背景の星とか、かもめとかを設置するコードも記載したい。
+  }
+});
